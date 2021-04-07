20494926b3bbddac6bd8443603b752f85e8789bd
Rohit Reddy Kotha <rohitkotha10@gmail.com>
2021-04-07 20:04:49 +0530

    Update rep

--- boiler_plate.cpp
+++ boiler_plate.cpp
@@ -2,7 +2,7 @@
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define int long long
 #define endl '\n'
-#define rep(i, start, endin, incre) for(int i = start; i < endin; i += incre)
+#define rep(i, start, endin, incre) for(int i = start; i <= endin; i += incre)
 using namespace std;
 
 void solve()
