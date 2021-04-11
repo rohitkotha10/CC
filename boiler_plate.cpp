f3a26154205e7ede16512e47eff0798fccd80b41
Rohit Reddy Kotha <rohitkotha10@gmail.com>
2021-04-12 04:32:17 +0530

    Add Date/time Extension

--- boiler_plate.cpp
+++ boiler_plate.cpp
@@ -1,9 +1,14 @@
+/*
+author:  rohitkotha10
+created: 12.04.2021 04:30:57
+*/
 #include <bits/stdc++.h>
+using namespace std;
+
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define int long long
 #define endl '\n'
 #define rep(i, start, endin, incre) for(int i = start; i <= endin; i += incre)
-using namespace std;
 
 void solve()
 {
