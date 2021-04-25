bc8f896e0687ec7083d1ebc53748860719a4d536
Rohit Reddy Kotha <rohitkotha10@gmail.com>
2021-04-25 17:31:18 +0530

    Update settings

---
+++ practice/B_Morning_Jogging.cpp
@@ -0,0 +1,32 @@
+/*
+author:  rohitkotha10
+created: 25.04.2021 17:23:17
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define int long long
+#define endl '\n'
+#define rep(i, start, endin, incre) for(int i = start; i <= endin; i += incre)
+
+const int MAX = 2e5 + 5;
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    
+}
+
+signed main()
+{
+    IOS;
+    int t=1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+}
